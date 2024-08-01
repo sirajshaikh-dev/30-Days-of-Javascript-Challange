28556ac065627cf2b25078501ae0a4623933ada1
sirajshaikh-dev <ss1425306@gmail.com>
2024-08-01 21:20:14 +0530

    Day8: ES6+ features
    complete Day8

--- Day8.js
+++ Day8.js
@@ -38,6 +38,43 @@ const {title,author}=book
 //Task5: Use the spread operator to create a new array that includes all elements of an existing array plys additional elements, and log the new array to console.
 const num1= [1,2,3,4,5]
 const newArr= [...num1,6,7,8]
-console.log(newArr) 
+// console.log(newArr) 
 
 //Task6: Use the rest opereator in a function to accept an arbitrary number of arguments,sum them, and return the result.
+function sum(...numbers) {
+    return numbers.reduce((total,num)=> total+num,0)
+}
+// console.log(sum(1,2,3))
+
+// Activity4: Default params
+//Task7: Write a function that takes two parameters and returns their product, with the second parameter having a default value of 1. log the result of calling this function with and without the second param.
+
+function product(num1,num2=1) {
+    return num1*num2;
+}
+// console.log(product(4)) // without second param
+// console.log(product(4,5))
+
+//Activity5: Enhanced Object Literals
+//Task8: Use enhanced object literals to create an object with methods and properties, and log the object to console.
+
+const carName= "Tata Motors";
+const model= "harrier";
+const car= {
+    carName,
+    model,
+     vehicle() {
+         console.log(` ${this.name} present  ${this.model}`)
+    },
+}
+console.log(car);
+// car.Vehicle()
+
+//Task9: Create an object with computed property names based on variables and log object to console
+
+ const key= "Laptop";
+ const value= "asus";
+ const obj={
+    [key]:value
+ }
+ console.log(obj)
