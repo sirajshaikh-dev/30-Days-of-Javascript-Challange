29bf33c7efdd4331b5ff2879be4f9e4d7853c588
sirajshaikh-dev <ss1425306@gmail.com>
2024-08-08 14:19:18 +0530

    Day10 Complete

--- Day10/Day10.js
+++ Day10/Day10.js
@@ -14,20 +14,27 @@ document.getElementById('btn1').addEventListener('click',function(){
 //Task2:Add a double click event listener to an image that toggles its visibility 
 const image= document.getElementById('task2')
 
-image.addEventListener('toggle',function(){
-     alert('clicked')
+image.addEventListener('dblclick',function(){
+    // if (image.style.display === 'none') {
+    //     image.style.display = 'block';
+    //   } else {
+    //     image.style.display = 'none';
+    //   }
+      //using ternary operator
+      this.style.display = this.style.display === "none" ? "block" : "none";
+  
 })
 
 //Activity2: Mouse events
 //Task3: Add a mouseover event listener to an element that changes its background color
 const element= document.getElementById('mouseEvent')
-// element.addEventListener('mouseover',function  ( ) {
-//     element.style.backgroundColor= 'blue'
-// })
+element.addEventListener('mouseover',function  ( ) {
+    element.style.backgroundColor= 'blue'
+})
 // //Task4: Add a mouseout event listener to an element that reset its bgcolor
-// element.addEventListener('mouseout',function(){
-//     element.style.backgroundColor= ""
-// })
+element.addEventListener('mouseout',function(){
+    element.style.backgroundColor= ""
+})
 
 //using toggle
 // function toggleBgColor(event) {
@@ -43,14 +50,53 @@ const element= document.getElementById('mouseEvent')
 //Activity3: Keyboard Events
 //Task5: Add a keydown event listener to an input field that logs the key pressed to the console.
 
-// const inputField= document.getElementById('task5')
-//  inputField.addEventListener('keydown',function () {
-//     console.log('keydown pressed')
-// })
+const inputField= document.getElementById('task5')
+ inputField.addEventListener('keydown',function () {
+    console.log('keydown pressed')
+})
 
 //Task6: Add a keyup event listener to an input field that displays the current value in paragraph
-const inputField= document.getElementById('task6')
+const inputField2= document.getElementById('task6')
 const para= document.getElementById('display')
-inputField.addEventListener('keyup',function(){
-    para.textContent= inputField.value;
+inputField2.addEventListener('keyup',function(){
+    para.textContent= `you typed: ${inputField2.value}`;
 })
+
+//Activity5: Form Events
+//Task7: Add a submit event listener to a form that prevents the defauld submission and logs the form data to the console.
+
+document.getElementById('form').addEventListener('submit',function(e){
+    e.preventDefault()
+    console.log("Default prevented")
+})
+
+//Task8: Add a change event listener a select dropdown that displays the selected value in paragraph.
+document.getElementById('lang').addEventListener('change',function(){
+    document.getElementById('displayPara').innerText=`Selected: ${this.value}`
+})
+
+//Activity5: Event Delegation: 
+//Task9:Add a click event listener to a list that logs the text content of the clicked list item using event delegation.
+const li= document.getElementById('task9')
+li.addEventListener('click',function(e){
+    if (e.target.tagName==='LI') {
+        console.log(e.target.textContent)
+    }
+})
+
+//Task10: Add an event listener to a parent element that listens for events from dynamically added child elements.
+const parentList = document.getElementById('parentList');
+const addItemButton = document.getElementById('addItem');
+ 
+function addItem() {
+    const newItem = document.createElement('li');
+    newItem.textContent = `Item ${parentList.children.length + 1}`;
+    parentList.appendChild(newItem);
+  }
+  parentList.addEventListener('click', function(event) {
+     if (event.target.tagName === 'LI') {
+       console.log(event.target.textContent);
+    }
+    
+});
+addItemButton.addEventListener('click', addItem);
