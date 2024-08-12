c938c516a989d1f00d74cecb7eebefec51c48444
sirajshaikh-dev <ss1425306@gmail.com>
2024-08-12 23:10:01 +0530

    Complete Day11.js
    Day11 Promise & Async-await

--- Day11/Day11.js
+++ Day11/Day11.js
@@ -104,3 +104,104 @@ fetchData('Fetching data from server1', 1000)
     //Activity 3: using async/await
     // Task4: Write an async function that waits for a promise to resolve and then logs the resolved value.
     
+    const getUserDetails = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve({ username: "iamvkr", lang: "js" });
+    }, 1000);
+})
+async function server() {
+    const result = await getUserDetails;
+    console.log("User Result: ", result);
+}
+server();
+
+// Task 5: Write an async function that handles a rejected promise using try-catch and logs the error message.
+const getUserDetails2 = new Promise((resolve, reject) => {
+    let err = true;
+    if (err) {
+        reject({ message: "internal server error" });
+        return false;
+    }
+    setTimeout(() => {
+        resolve({ username: "iamvkr", lang: "js" });
+    }, 2000);
+})
+async function server2() {
+    try {
+        const result = await getUserDetails2;
+        console.log("User Result: ", result);
+    } catch (error) {
+        console.log("ERR",error);
+    }
+}
+server2();
+
+// Activity 4: Fetching Data from an API
+
+// Task 6: Use the fetch API to get data from a public API and log the response data to the console using promises.
+
+const pm = new Promise((resolve, reject) => {
+    fetch('https://dummyjson.com/products/1')
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err))
+})
+pm.then(data => console.log(data))
+
+// Task 7: Use the fetch API to get data from a public API and log the response data to the console using async/await.
+const getData = async (url) => {
+    try {
+        const res = await fetch(url)
+        const data = await res.json(url);
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+getData('https://dummyjson.com/products/1');
+
+// Activity 5: Concurrent Promises
+
+// Task 8: Use Promise.all to wait for multiple promises to resolve and then log all their values.
+const p1 = new Promise((res,rej)=>{
+    setTimeout(() => {
+        res("P1 is done")
+    }, 1000);
+})
+const p2 = new Promise((res,rej)=>{
+    setTimeout(() => {
+        res("P2 is done")
+    }, 1500);
+})
+const p3 = new Promise((res,rej)=>{
+    setTimeout(() => {
+        res("P3 is done")
+    }, 2000);
+})
+
+const result =  Promise.all([p1,p2,p3]);
+result.then(res=>{
+    console.log(`Result of p1: ${res[0]}`);
+    console.log(`Result of p2: ${res[1]}`);
+    console.log(`Result of p3: ${res[2]}`);
+});
+// Task 9: Use Promise.race to log the value of the first promise that resolves among multiple promises.
+const promise1 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("Promise 1 resolved"), 500);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("Promise 2 resolved"), 300);
+});
+
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("Promise 3 resolved"), 1000);
+});
+
+Promise.race([promise1, promise2, promise3])
+  .then(value => {
+    console.log(value); // Logs "Promise 2 resolved" because it resolves the fastest
+  })
+  .catch(error => {
+    console.error(error);
+  });
